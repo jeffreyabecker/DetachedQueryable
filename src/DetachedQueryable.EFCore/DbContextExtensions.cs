5f53633729ee55d55ee9824fdae031c97489222b
Jeffrey Becker <jabecker@jmt.com>
2017-01-27 08:49:23 -0500

    added EFCore Support

---
+++ src/DetachedQueryable.EFCore/DbContextExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace DetachedQueryable.EFCore
+{
+    public static class DbContextExtensions
+    {
+        public static IQueryable<T> Attach<T>(this DbContext ctx, IQueryable<T> detachedQuery)
+        {
+            return DetachedQuery.Attach(detachedQuery, new EfQueryableFactory(ctx));
+        }
+
+
+    }
+}
---
+++ src/DetachedQueryable.EFCore/EfQueryableFactory.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace DetachedQueryable.EFCore
+{
+    public class EfQueryableFactory : IQueryableFactory
+    {
+        private readonly DbContext _ctx;
+
+        public EfQueryableFactory(DbContext ctx)
+        {
+            _ctx = ctx;
+        }
+
+        public IQueryable<T> GetQueryable<T>() where T : class
+        {
+            return _ctx.Set<T>();
+        }
+    }
+}
